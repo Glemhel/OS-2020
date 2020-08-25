a491bcc94ae22d71948106c62887fc6b9e340231
Glemhel <ya.rudakov2001@yandex.ru>
2020-08-25 15:45:53 +0300

    Lab 2 task 2

---
+++ week2/ex2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LENGTH 105
+
+
+int main(){
+    char str[MAX_LENGTH];
+    printf("Input a string of reasonable size (<100):\n");
+    fgets(str, MAX_LENGTH, stdin);
+    int length = strlen(str);
+    printf("Here is your string reversed:\n");
+    for (int i = 1; i < length; i++){
+        putchar(str[length - i - 1]);
+    }
+    printf("\n");
+    return 0;
+}
