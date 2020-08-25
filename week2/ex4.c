063aa31a294420030ec309174acc67740801c992
Glemhel <ya.rudakov2001@yandex.ru>
2020-08-25 17:28:37 +0300

    Lab 2 task 4

---
+++ week2/ex4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void swap(int* a, int* b){
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+int main(){
+    printf("Enter two integers separated by a space character:\n");
+    int a, b;
+    scanf("%d %d", &a, &b);
+    swap(&a, &b);
+    printf("Here are your numbers swapped:\n%d %d\n", a, b);
+    return 0;
+}
